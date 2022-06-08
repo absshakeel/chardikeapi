228ed5520a82773e07c99d269e61944cc0d8e97c
thinkwithabs <twithabs@gmail.com>
2022-06-08 18:59:55 +0600

    url patersn change

--- MainApplication/urls.py
+++ MainApplication/urls.py
@@ -52,5 +52,8 @@ urlpatterns = [
 ]
 
 
-urlpatterns+= staticfiles_urlpatterns()
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT))
