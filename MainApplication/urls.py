d57103fee2fbd92c1587f5fee3ec99de6f2f6c4b
thinkwithabs <twithabs@gmail.com>
2022-06-08 21:06:43 +0600

    media

--- MainApplication/urls.py
+++ MainApplication/urls.py
@@ -56,7 +56,7 @@ urlpatterns = [
 #                           document_root=settings.STATIC_ROOT)
 #     urlpatterns += static(settings.MEDIA_URL,
 #                           document_root=settings.MEDIA_ROOT)
-# urlpatterns+= staticfiles_urlpatterns()
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+= static('/media/', document_root=settings.MEDIA_ROOT)
 # # if settings.DEBUG:
 # #     urlpatterns += static(settings.STATIC_URL,  document.root=settings.STATIC_ROOT)
