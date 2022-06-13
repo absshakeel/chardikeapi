2d097be319a403c92a8142339700f9dff8e2622d
thinkwithabs <twithabs@gmail.com>
2022-06-13 12:27:12 +0600

    stattic root added

--- MainApplication/settings.py
+++ MainApplication/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = STATIC_DIR
 STATIC_URL = 'static/'
 STATIC_DIRS = [STATIC_DIR,]
 
