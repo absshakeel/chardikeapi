5dd06f56586e8841fd04726647b4624fda55c2d7
thinkwithabs <twithabs@gmail.com>
2022-07-26 18:17:18 +0600

    ff

--- flash_sale/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlashSaleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flash_sale'
-
-    def ready(self):
-        import flash_sale.signals
