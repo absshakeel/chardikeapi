2bb6487218935cfac1b33fb3f555b8ffcb1b0409
thinkwithabs <twithabs@gmail.com>
2022-07-26 17:27:17 +0600

    fsjdlkfs

--- flash_sale/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from flash_sale.models import (
-    FlashSale,FlashProducts
-)
-
-# Register your models here.
-admin.site.register(FlashSale)
-admin.site.register(FlashProducts)
