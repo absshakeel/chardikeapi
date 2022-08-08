be9f12dc456a50714006b439bae08c83f0be2204
thinkwithabs <twithabs@gmail.com>
2022-08-08 22:31:21 +0600

    ll

--- MainApplication/settings.py
+++ MainApplication/settings.py
@@ -242,8 +242,8 @@ Use of Django Simple JWT  Token Authentication
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+     'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
